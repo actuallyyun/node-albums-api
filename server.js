13e7d05ab3e14b2f5c2579f6aff0a50afe782f46
Yun Ji <this.jiyun@gmail.com>
2022-11-29 21:12:53 +0100

    feature:add /albums route

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express')
 
 const app = express();
 
+const albums = require("./albums.json")
+
 app.get('/', (req, res) => {
     res.send('Hello Express')
 });
 
 
-
+app.get("/albums", (req, res) => {
+    res.send(albums)
+})
 
 
 app.listen(3000, () => console.log("Server is up and running"))
